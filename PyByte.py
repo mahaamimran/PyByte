34f397735dc5f2a3f5977eaad96b880002a3e5af
mahaamimran <mahaamimran@gmail.com>
2023-07-14 14:04:39 +0500

    user input

--- PyByte.py
+++ PyByte.py
@@ -1,10 +1,23 @@
 from PIL import Image
 
-# Open the JPEG image
-image = Image.open('input.jpg')
+def compress_image(quality):
+    # Open the JPEG image
+    image = Image.open('input.jpg')
 
-# Set the desired compression quality (1-100)
-compression_quality = 15
+    # Save the compressed image as a new JPEG file
+    image.save('compressed.jpg', quality=quality)
 
-# Save the compressed image as a new JPEG file
-image.save('compressed.jpg', quality=compression_quality)
+print("Welcome to PyByte")
+print("What do you want to compress?")
+print("1. Image")
+choice = int(input())
+
+if choice == 1:
+    # Ask for compression quality
+    quality = int(input("Enter the compression quality (1-100): "))
+    # Validate the input
+    if quality < 1 or quality > 100:
+        print("Invalid compression quality. Please enter a value between 1 and 100.")
+    else:
+        compress_image(quality)
+        print("Image compressed successfully!")
